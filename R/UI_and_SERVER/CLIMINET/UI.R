86601ee8833034d9c2266f1df9b7fdc982fa5e8d
KYLLIAN JAMES <98753607+kiki1801@users.noreply.github.com>
2024-10-04 15:42:22 +0200

    Update UI.R

--- R/UI_and_SERVER/CLIMINET/UI.R
+++ R/UI_and_SERVER/CLIMINET/UI.R
@@ -2,7 +2,7 @@
 #####                Climate ChanGe MiGration Network                #####
 ##### Fondation Jean-Jacques Laffont - Toulouse Sciences Economiques #####
 #####                   KYLLIAN JAMES | 2023-11-14                   #####
-#####                        Mod.: 2024-10-02                        #####
+#####                        Mod.: 2024-10-03                        #####
 #####                               UI                               #####
 ##########################################################################
 
