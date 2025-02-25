f352fbc6cd7f7d8e3d2453de7b73e58674721aea
KYLLIAN <98753607+kiki1801@users.noreply.github.com>
2025-02-25 16:36:23 +0100

    Update Coordinates.R

--- R/Coordinates.R
+++ R/Coordinates.R
@@ -24,19 +24,19 @@ ui <- fluidPage(
   titlePanel("Coordinates"),
   
   ##### CSS Code #####
-  tags$style(".leaflet-container { cursor: crosshair !important; }"),
+  tags$style(".leaflet-container {cursor: crosshair !important;}"),
   
   ##### World Map #####
-  leafletOutput("world_map", width = "100%", height = 450),
+  leafletOutput("world_map", width = "100%", height = 510),
   #MARGIN (Top: 10px)
   tags$div(style = "margin-top: 10px;"),
-  ##### CLEAR VALUE(s) ACTION BUTTON #####
+  ##### Action Button => Clear Values #####
   actionButton(inputId = "clear_values", label = "Clear Values"),
-  ##### CLEAR ONE VALUE ACTION BUTTON #####
+  ##### Action Button => Clear One Value #####
   actionButton(inputId = "clear_one_value", label = "Clear Value [-1]"),
   #MARGIN (Top: 10px)
   tags$div(style = "margin-top: 10px;"),
-  ##### TEXT ####
+  ##### Coordinates ####
   verbatimTextOutput("coordinates")
   
   )
@@ -47,37 +47,49 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
+  ##### Reactive Values (RV) #####
   RV <- reactiveValues(LATITUDE_VALUEs = numeric(0), LONGITUDE_VALUEs = numeric(0))
   
+  ##### World Map Tiles #####
   output$world_map <- renderLeaflet({
     
     leaflet() %>%
-      addTiles(options = tileOptions(minZoom = 1, maxZoom = 18, maxNativeZoom = 18)) %>%
-      # addTiles(options = tileOptions(minZoom = 8, maxZoom = 13, maxNativeZoom = 18)) %>%
+      #Add Tile LAYER
+      # addTiles(options = tileOptions(minZoom = 1, maxZoom = 18, maxNativeZoom = 18)) %>% #Min/Max Zoom Levels
+      #Add Tile LAYER
+      # addTiles(options = tileOptions(minZoom = 8, maxZoom = 13, maxNativeZoom = 18)) %>% #Min/Max Zoom Levels
+      #Add Tile LAYER
+      # addTiles(options = tileOptions(minZoom = 1, maxZoom = 10, maxNativeZoom = 18)) %>% #Min/Max Zoom Levels
+      #Add Tile LAYER
+      addTiles(options = tileOptions(minZoom = 8, maxZoom = 15, maxNativeZoom = 18)) %>% #Min/Max Zoom Levels
       addScaleBar(position = "bottomleft") %>% 
+      #World Map View => CENTER => LONGITUDE = 0 | LATITUDE = 0 | Zoom Level = 1
       setView(lng = 0, lat = 0, zoom = 1) %>%
-      addDrawToolbar(
-        position = "topleft", 
-        polylineOptions = FALSE, circleOptions = FALSE, rectangleOptions = FALSE, 
-        markerOptions = FALSE, circleMarkerOptions = FALSE,
-        polygonOptions = drawPolygonOptions(shapeOptions = drawShapeOptions(
-          stroke = TRUE, color = "#0000FF", weight = 1.5, opacity = 0.75, 
-          fill = TRUE, fillOpacity = 0.25)), 
-        editOptions = editToolbarOptions(edit = FALSE, remove = TRUE))
+      addDrawToolbar(position = "topleft", #ToolBAR => POSITION
+                     polygonOptions = drawPolygonOptions(shapeOptions = drawShapeOptions(
+                       #Stroke Attributes | Fill Attributes
+                       stroke = TRUE, color = "#0000FF", weight = 1.5, opacity = 0.75, fill = TRUE, fillOpacity = 0.25)), 
+                     editOptions = editToolbarOptions(edit = FALSE, remove = TRUE))
     
         })
   
-  observeEvent(input$world_map_click, {
+  ##### World Map => Click #####
+  observeEvent(input$world_map_click, { #WHEN World Map is clicked
     
+    #Click => LATITUDE | LONGITUDE
     click <- input$world_map_click
     
+    #Reactive Value => Store LATITUDE Values
     RV$LATITUDE_VALUEs <- c(RV$LATITUDE_VALUEs, round(click$lat, 5))
+    #Reactive Value => Store LONGITUDE Values
     RV$LONGITUDE_VALUEs <- c(RV$LONGITUDE_VALUEs, round(click$lng, 5))
     
     })
   
+  ##### Coordinates #####
   output$coordinates <- renderPrint({
     
+    #LATITUDE Values
     cat("LATITUDE_VALUEs <- c(")
     for (i in seq_along(RV$LATITUDE_VALUEs)) {
       if (i > 1) {
@@ -85,6 +97,7 @@ server <- function(input, output, session) {
       cat(round(RV$LATITUDE_VALUEs[i], 5))}
     cat(")")
     
+    #LONGITUDE Values
     cat("\nLONGITUDE_VALUEs <- c(")
     for (i in seq_along(RV$LONGITUDE_VALUEs)) {
       if (i > 1) {
@@ -94,16 +107,22 @@ server <- function(input, output, session) {
     
     })
   
-  observeEvent(input$clear_values, {
+  ##### Action Button => Clear Values #####
+  observeEvent(input$clear_values, { #WHEN Action Button is clicked
     
+    #Reactive Value => Clear LATITUDE Values
     RV$LATITUDE_VALUEs <- numeric(0)
+    #Reactive Value => Clear LONGITUDE Values
     RV$LONGITUDE_VALUEs <- numeric(0)
     
     })
   
-  observeEvent(input$clear_one_value, {
+  ##### Action Button => Clear One Value #####
+  observeEvent(input$clear_one_value, { #WHEN Action Button is clicked
     
+    #Reactive Value => Clear One LATITUDE Value
     RV$LATITUDE_VALUEs <- head(RV$LATITUDE_VALUEs, -1)
+    #Reactive Value => Clear One LONGITUDE Value
     RV$LONGITUDE_VALUEs <- head(RV$LONGITUDE_VALUEs, -1)
     
     })
