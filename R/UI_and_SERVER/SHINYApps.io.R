e604cc5feb4381fc7c44df7f36fec7ed3e60bbee
KYLLIAN <98753607+kiki1801@users.noreply.github.com>
2025-03-03 16:18:25 +0100

    Update SHINYApps.io.R

--- R/UI_and_SERVER/SHINYApps.io.R
+++ R/UI_and_SERVER/SHINYApps.io.R
@@ -3,7 +3,7 @@
 ####################################
 
 #CONFIGURE Account
-rsconnect::setAccountInfo(name='climinet', token='16C4545A62B56510770407F173C363C9', secret='W9il7/nNTFORgmBDJs+xy+0sVDkSYTktHtYftExD', server = 'shinyapps.io')
+rsconnect::setAccountInfo(name='climinet', token='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', secret='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', server = 'shinyapps.io')
 
 #DEPLOY SHINY Application To SHINYApps.io SERVER
 rsconnect::deployApp(appDir = paste0(getwd(), "/R/UI_and_SERVER/CLIMINET"), appName = 'demo', appMode = 'shiny', contentCategory = 'site', account = 'climinet', server = 'shinyapps.io')
